7ef54f337e11f037b5a619ab26eb9b1628a2dd07
HyunJoo Kim <hyunjoogo@gmail.com>
2022-05-15 18:23:44 +0900

    LS-6 테이블 삭제 버튼 누르면 모달창 나오도록 구현

--- history-list.js
+++ history-list.js
@@ -1,3 +1,6 @@
+// eslint-disable-next-line import/extensions
+import ModalHandler from './modalClass.js';
+
 const dummyData = [
   {
     id: 1,
@@ -37,6 +40,8 @@ const dummyData = [
 
 const tableList = document.querySelector('.data-table');
 
+let modalState = false;
+
 // fetch().then();
 
 function makeTableData(data) {
@@ -44,31 +49,43 @@ function makeTableData(data) {
   const tr = document.createElement('tr');
 
   // td 만들기
-  for (const i in data) {
-    if (i === 'id') {
-      tr.className = `tr-${data[i]}`;
-      continue;
+  Object.keys(data).forEach((key) => {
+    if (key === 'id') {
+      tr.className = `tr-${data[key]}`;
+      tr.setAttribute('data-id', data[key]);
+    } else {
+      const tableData = document.createElement('td');
+      tableData.className = `user-${key}`;
+      tableData.textContent = data[key];
+      tr.appendChild(tableData);
     }
-    const tableData = document.createElement('td');
-    tableData.className = `user-${i}`;
-    tableData.textContent = data[i];
-    tr.appendChild(tableData);
-  }
-
+  });
+  const tableData = document.createElement('td');
+  const deleteBtn = document.createElement('button');
+  deleteBtn.textContent = '삭제';
+  deleteBtn.className = 'history-deleteBtn';
+  deleteBtn.addEventListener('click', () => {
+    console.log('삭제해야할 id', tr.getAttribute('data-id'));
+    modalState = true;
+    const options = { size: 'sm', backgroundColor: 'red' };
+    const a = new ModalHandler(modalState, data, options);
+    a.makeModalContent();
+    a.open();
+  });
+  tableData.appendChild(deleteBtn);
+  tr.appendChild(tableData);
   tableList.appendChild(tr);
-  /*
-  <tr>
-    <td>김현주</td>
-    <td>10월 27일</td>
-    <td>연차</td>
-    <td>오후 반차</td>
-    <td>0.5일</td>
-    <td>13일</td>
-    <td>그냥</td>
-  </tr>
-  */
 }
 
-dummyData.map((value) => {
+// 모달만 띄우는 놈
+// 모달 내용만 만들어주는 놈
+
+dummyData.forEach((value) => {
   makeTableData(value);
 });
+
+// 눌렀을때 상세 모달창이 나오도록
+
+// 1. 모달창을 각 화면에서 하나씩 추가를 해줄 것이냐?
+// 2. 반복되는거니까...?
+// 컴포넌트화 시키자
